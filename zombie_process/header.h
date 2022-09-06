8ea2e9791e4764803cfc3751bd159c637d54afdd
krushnparmar <kparmar2101@gmail.com>
2022-09-06 14:45:57 +0530

    zombie process

---
+++ zombie_process/header.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+// #include <sys/time.h>
+// #include <sys/resource.h>
+#include <sys/types.h>
+// #include <sys/stat.h>
+#include <unistd.h>
---
+++ zombie_process/zombie.c
@@ -0,0 +1,20 @@
+#include "header.h" 
+
+//zombie processes are harmful because even after execution 
+//child process will still occupy the ram.....
+
+int main() {
+    if(fork()==0) {
+        printf("in child process pid=%d, ppid = %d\n", getpid(), getppid());
+        sleep(10);
+        printf("in child process pid=%d, ppid = %d\n", getpid(), getppid());    
+        //Here after 10 seconds process manager(or kernel) will make this 
+        //child process as dead or zombie process
+    }
+    else {
+        while(1);
+    }
+    //child process will be zombie process or defunc process 
+    //because parent needs to collect exit status of child
+    //Once parent gets exit status of child process manager removes child from zombie.
+}
